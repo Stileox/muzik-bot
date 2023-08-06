d2159edf33e5c4737e1438ec170939331e78286c
Stileox <113307558+Stileox@users.noreply.github.com>
2023-08-06 20:17:23 +0300

    Delete settings directory

--- settings/ayarlar.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-
-module.exports = {
-    TOKEN: process.env.TOKEN || "MTAxNjA1MjI5OTkzMzcwMDIxNg.Gj9vHM.BySk5YQPqcym8dvr22gs2ucrQnpSEJVSZ46MxY",  // lrows token
-    PREFIX: process.env.PREFIX || "!", // lrows prefix
-    SAHİP: process.env.SAHİP || "898281297796005888", //lrows id
-}
