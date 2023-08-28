ae554a996d5be2f017e87c48ee37d5a7890e87b2
Stileox <113307558+Stileox@users.noreply.github.com>
2023-08-28 18:34:30 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    token: "MTE0NTY5OTA4NDk3NjUyOTQyOQ.GYR6Ow.JMqAr2ol3nAeknamoS0gPI_U4TQhuP94Alwbu4",
+    id: "1145699084976529429",
+    name: "AlfaBot",
+    premiumroleid: "1145701930543358013",
+    logkanalid: "1145701761575813240"
+}
